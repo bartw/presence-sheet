56f87ac2c221dc139ba307b7dcfa9a527c1c5ad8
Bart Wijnants <bartwijnants@outlook.com>
2017-04-01 10:10:12 +0200

    react hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    React.createElement('h1', null, 'Hello world!'),
+    document.getElementById('root')
+);
