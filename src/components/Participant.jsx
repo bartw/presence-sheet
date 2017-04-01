367a19f5cf64f0c6ae9776cbcb7cf8e5953623f0
Bart Wijnants <bartwijnants@outlook.com>
2017-04-01 10:37:32 +0200

    conditional rendering

--- src/components/Participant.jsx
+++ src/components/Participant.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 export default class Participant extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { isCegekaEmployee: false }
     }
 
     render() {
-        return <li>{this.props.name}</li>;
+        return <li>{this.props.name} {this.state.isCegekaEmployee && <span>&#10004;</span>}</li>;
     }
 }
