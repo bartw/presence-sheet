e9abd40387f13087e5ebb044a52444b07747c240
Bart Wijnants <bartwijnants@outlook.com>
2017-04-01 10:20:28 +0200

    props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import Header from './Header';
 
 export default function App () {
-    return <Header />;
+    return <Header name="Bart" />;
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header({ name }) {
     return (
         <header>
-            <h1>Hello Bart!</h1>
+            <h1>Hello {name}!</h1>
         </header>
     );
 }
