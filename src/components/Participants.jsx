6b48a0ee2b1c1ce75c41812bdfb292e24f48199f
Bart Wijnants <bartwijnants@outlook.com>
2017-04-01 10:29:04 +0200

    extract component

--- src/components/Participants.jsx
+++ src/components/Participants.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Participant from './Participant';
+
 
 export default function Participants({ participants }) {
-    const items = participants.map(participant => <li key={participant}>{participant}</li>);
+    const items = participants.map(participant => <Participant key={participant} name={participant} />);
     return (
         <ul>
             {items}
