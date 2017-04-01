41161a1f95b30530ffbb7a9e9b234085cffa4d52
Bart Wijnants <bartwijnants@outlook.com>
2017-04-01 10:45:58 +0200

    state

--- src/components/Participant.jsx
+++ src/components/Participant.jsx
@@ -4,9 +4,20 @@ export default class Participant extends React.Component {
     constructor(props) {
         super(props);
         this.state = { isCegekaEmployee: false }
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState(prevState => ({ isCegekaEmployee: !prevState.isCegekaEmployee }));
     }
 
     render() {
-        return <li>{this.props.name} {this.state.isCegekaEmployee && <span>&#10004;</span>}</li>;
+        return (
+            <li>
+                <button onClick={this.toggle}>Toggle</button>
+                <span>{this.props.name}</span>
+                {this.state.isCegekaEmployee && <span>&#10004;</span>}
+            </li>
+        );
     }
 }
